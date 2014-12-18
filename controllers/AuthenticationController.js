30abe634c8beaeb89b623f1b93fdc61d2f15fb61
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-18 17:18:21 -0600

    testing...

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -7,19 +7,49 @@ var authenticateUser = function(req, res){
 	var emailAddress = req.body.emailAddress;
 	var password = req.body.password;
 
-	authenticateUserInternal(emailAddress, password).then(function(authenticatedUser){
-		if ( authenticatedUser ){
-			res.send({ success:true, user: authenticatedUser });
+	var accessToken = req.body.accessToken;
+
+	// try the access token first
+	if ( accessToken ){
+		authenticateUserAccessToken(accessToken).then(function(authenticatedUser){
+			if ( authenticatedUser ){
+				res.send({ success:true, user: authenticatedUser });
+			}
+			else{
+				res.send({success:false, user: null});
+			}
+		});
+	}
+	else{
+		// try email and password auth
+		authenticateUserUsernamePassword(emailAddress, password).then(function(authenticatedUser){
+			if ( authenticatedUser ){
+				res.send({ success:true, user: authenticatedUser });
+			}
+			else{
+				res.send({success:false, user: null});
+			}
+		});
+	}
+};
+
+var authenticateUserAccessToken = function(token){
+	var deferred = Q.defer();
+
+	// get the user
+	userService.getUserByToken(token).then(function(user){
+		if ( user && token == user.accessToken ){
+			deferred.resolve(user);
 		}
-		else{
-			res.send({success:false, user: null});
-		}
+		deferred.resolve(null);
 	});
+
+	return deferred.promise;
 };
 
 /* Private Methods */
 
-var authenticateUserInternal(emailAddress, password){
+var authenticateUserUsernamePassword = function(emailAddress, password){
 	var deferred = Q.defer();
 
 	// get the user
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var authenticationController = require('./controllers/AuthenticationController');
 
 var service = require("soa-example-core-service");
-var config = require("soa-example-core-service-config").config();
+var config = require("soa-example-service-config").config();
 
-var app = service.createServer(config.authenticationServiceIp);
+var app = service.createApiServer(config.authenticationServicePort);
 
 app.post('/authenticate', authenticationController.authenticateUser);
