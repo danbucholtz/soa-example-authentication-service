03bb8f526c0a39da897bb3a0b07887f005af7d46
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-29 12:39:56 -0600

    updates

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -1,6 +1,8 @@
 var userService = require("soa-example-user-service-api");
 var utils = require("soa-example-core-utils");
 
+//var //loggerServiceApi = require('soa-example-logging-service-api');
+
 var Q = require("q");
 
 var authenticateUser = function(req, res){
@@ -36,11 +38,15 @@ var authenticateUser = function(req, res){
 var authenticateUserAccessToken = function(token){
 	var deferred = Q.defer();
 
+	//loggerServiceApi.debug("system", "Access Token [" + token + "] authenticating ...");
+
 	// get the user
 	userService.getUserByToken(token).then(function(user){
 		if ( user && token == user.accessToken ){
+			//loggerServiceApi.debug("system", "Access Token [" + token + "] authenticating ... Success");
 			deferred.resolve(user);
 		}
+		//loggerServiceApi.debug("system", "Access Token [" + token + "] authenticating ... Fail");
 		deferred.resolve(null);
 	});
 
@@ -52,15 +58,19 @@ var authenticateUserAccessToken = function(token){
 var authenticateUserUsernamePassword = function(emailAddress, password){
 	var deferred = Q.defer();
 
+	//loggerServiceApi.debug("system", "User [" + emailAddress + "] authenticating ...");
+
 	// get the user
 	userService.getUserByEmailAddress(emailAddress).then(function(user){
 		if ( user ){
 			var hashedPassword = utils.hashPassword(password, user.salt);
 			if ( hashedPassword == user.password ){
+				//loggerServiceApi.debug("system", "User [" + emailAddress + "] authenticating ... Success");
 				deferred.resolve(user);
 				return;
 			}
 		}
+		//loggerServiceApi.debug("system", "User [" + emailAddress + "] authenticating ... Fail");
 		deferred.resolve(null);
 	});
 
